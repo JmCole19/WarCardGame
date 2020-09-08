5906cc76aad136ab0ffc2d578a6a81538cdd38a8
Jason Coleman <jayemcee121@gmail.com>
2020-09-08 11:29:33 -0400

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Jason Coleman on 9/8/20.
+//  Copyright © 2020 Jason Coleman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
